e5123bd60febe951125b2f2ac45b462665a0b285
Lee Miller <millerl2@hawkmail.newpaltz.edu>
2017-12-20 21:52:59 -0500

    Final Commit before final. Facebook login impelmented but app still in development. Login info: Username = "lee" Password = "mill"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,4 +22,8 @@ export class LoginComponent implements OnInit {
     
   }
 
+  loginF(){
+    this.exercise.loginFB();
+  }
+
 }
--- src/app/models/exercise.service.ts
+++ src/app/models/exercise.service.ts
@@ -13,7 +13,46 @@ export class ExerciseService {
 
   ngOnInit(){}
 
-  constructor(private http: Http, private router: Router) {}
+  constructor(private http: Http, private router: Router) {
+    console.log("Ok well it tried");
+    window.fbAsyncInit = function() {
+      console.log("initializing");
+      FB.init({
+        appId      : '161753371106684',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.11'
+      });
+        
+      FB.AppEvents.logPageView();   
+        
+    };
+  
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = <HTMLScriptElement>d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  }
+
+
+  loginFB() {
+    FB.login((response: any) => {
+        if (response.authResponse) {
+         console.log(response);
+
+         FB.api('/me?fields=name,email,picture', (response: any) => {
+           console.log(response);
+           this.login(response.name, 'password', response.id, response.picture.data.url);
+         });
+
+        } else {
+         console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scopes: 'email,user_photos,user_posts'});
+}
 
   login(name: string, password: string, fbid?: string, picture?: string){
     console.log(name + " " + password + " " + fbid + " " + picture)
@@ -33,5 +72,4 @@ export class ExerciseService {
     )
     
 }
-
 }
